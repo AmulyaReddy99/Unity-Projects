c91fcf368253c3072c064caf3023460fc44c88b9
AmulyaReddy99 <amulyareddyk@yahoo.in>
2020-07-27 16:39:04 +0530

    complete game

---
+++ PassObstacles/Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	bool gameHasEnded = false;
+	public float restartDelay = 2f;
+	public GameObject completeLevelUI;
+
+	public void CompleteLevel(){
+		completeLevelUI.SetActive(true);
+	}
+
+	public void EndGame(){
+		if(!gameHasEnded){
+			gameHasEnded = true;
+			Invoke("Restart",restartDelay);
+		}
+	}
+	void Restart(){
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+}

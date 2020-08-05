565e0b91240151badd2fa94a1f5bad1d92eb0473
AmulyaReddy99 <amulyareddyk@yahoo.in>
2020-08-05 18:48:57 +0530

    city,rpg,scripts,colors

---
+++ RPG/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	public float forwardForce = 8000f;
+	public float sidewaysForce = 100f;
+
+	[SerializeField]
+	private float speed = 5f;
+	
+	void FixedUpdate () {
+
+		float _xMov = Input.GetAxis("Horizontal");
+		float _zMov = Input.GetAxis("Vertical");
+
+		Vector3 _movHorizontal = transform.right * _xMov;
+		Vector3 _movVertical = transform.forward * _zMov;
+
+		Vector3 velocity = (_movHorizontal + _movVertical) * speed;
+
+		if (velocity != Vector3.zero)
+			rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+
+	}
+}

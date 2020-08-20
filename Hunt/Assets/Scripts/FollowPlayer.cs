df87e1cb60d88d920f951cc53fb84e9e9b54bf21
AmulyaReddy99 <amulyareddyk@yahoo.in>
2020-08-20 17:47:07 +0530

    player sync, affect, shoot, camera follow correction

--- Hunt/Assets/Scripts/FollowPlayer.cs
+++ Hunt/Assets/Scripts/FollowPlayer.cs
@@ -3,12 +3,21 @@
 public class FollowPlayer : MonoBehaviour {
 
 	public GameObject player;
+	public Vector3 initial;
 	public Vector3 offset;
 	public float lookSensitivity = 3f;
+
+	bool first = true;
 	
 	void Update () {
 		
-		transform.position = GameObject.FindWithTag("Player").transform.position + offset;
+		// transform.position = GameObject.FindWithTag("Player").transform.position + offset;
+		// transform.position = GameObject.FindWithTag("Player").transform.position + offset;
+
+		// if(first){
+		// 	transform.eulerAngles = new Vector3(0, transform.eulerAngles.y+lookSensitivity,0);
+		// 	first = false;
+		// }
 		
 		if(Input.GetKey("d"))
 			transform.eulerAngles = new Vector3(0, transform.eulerAngles.y+lookSensitivity,0);
---
+++ Hunt/Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+	[SerializeField]
+	Behaviour[] componentsToDisable;
+
+	[SerializeField]
+	string remoteLayerName = "RemotePlayer";
+
+	void Start(){
+		if(!isLocalPlayer){
+			for(int i = 0; i<componentsToDisable.Length; i++)
+				componentsToDisable[i].enabled = false;
+			gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
+		}
+		else
+			Camera.main.gameObject.SetActive(false);
+	}
+}

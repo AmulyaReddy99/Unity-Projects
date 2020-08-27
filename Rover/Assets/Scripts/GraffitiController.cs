847d28339ba53259157a1c711c8ed9b7cead315e
AmulyaReddy99 <amulyareddyk@yahoo.in>
2020-08-27 23:49:11 +0530

    Graffiti added

---
+++ Rover/Assets/Scripts/GraffitiController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GraffitiController : MonoBehaviour {
+
+	bool equipped = false;
+
+	public Image icon;
+	public GameObject graffiti;
+	public Transform player;
+	float graffitiZOffset;
+	float graffitiYOffset;
+
+	void Start(){
+		graffitiYOffset = graffiti.transform.position.y - player.position.y;
+		graffitiZOffset = player.position.z - graffiti.transform.position.z;
+		graffiti.SetActive(false);
+	}
+
+	void FixedUpdate () {
+
+		if (Input.GetKeyDown(KeyCode.G)){
+			equipped = !equipped;
+			if (equipped){
+				icon.enabled = true;
+				graffiti.SetActive(true);
+				graffiti.transform.position = new Vector3(player.position.x, player.position.y + graffitiYOffset, player.position.z - graffitiZOffset);
+			}
+			else {
+				graffiti.SetActive(false);
+				icon.enabled = false;
+			}
+		}
+	}
+}
---
+++ Rover/Assets/Scripts/Paint.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Paint : MonoBehaviour {
+
+	[SerializeField]
+	private float lookRadius = 10f;
+
+	[SerializeField]
+	private Camera cam;
+
+	bool facing = true;
+	float distance;
+
+	void FixedUpdate(){
+
+		if (Input.GetMouseButtonDown(0)){
+
+			RaycastHit _hit;
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+			if (Physics.Raycast(ray, out _hit)){
+
+				if (_hit.collider.tag=="Terrain"){
+					Debug.Log("Collided " + transform.position + " " + _hit.point);
+					
+					// distance = new Vector3.Distance(transform.position, _hit.point);
+					// if (distance <= lookRadius && facing){
+					// 	Debug.Log(_hit.point);
+					// }
+				}
+			}
+		}
+	}
+}
